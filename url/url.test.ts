bd4b1a22bc3a501106339dc266cf1e7ccdf5ff0b
HerbertCodex <krakoffi57@gmail.com>
2025-03-10 22:29:37 +0100

    Initial commit

--- url/url.test.ts
+++ url/url.test.ts
@@ -5,7 +5,7 @@ import { get, shorten } from './url';
 
 describe('shorten', () => {
   test('getting a shortened url should give back the original', async () => {
-    const resp = await shorten({ url: 'https://examplre.com' });
+    const resp = await shorten({ url: 'https://example.com' });
     const url = await get({ id: resp.id });
     expect(url.url).toBe('https://example.com');
   });
